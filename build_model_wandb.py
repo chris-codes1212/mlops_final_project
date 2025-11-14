25ee3c16348649dca930af08cd45acd53251ef4d
Chris <chris.r.thompson1212@gmail.com>
2025-11-14 16:11:03 -0500

    making changes for better model

--- build_model_wandb.py
+++ build_model_wandb.py
@@ -9,6 +9,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.layers import Bidirectional, GlobalMaxPooling1D
 
 from sklearn.model_selection import train_test_split
 from skmultilearn.model_selection import iterative_train_test_split
@@ -22,23 +23,18 @@ tf.keras.mixed_precision.set_global_policy('mixed_float16')
 # create function to clearn the input text
 def clean_text(text):
     text = text.lower()
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
+
 # load data and create tensorflow dataset objects for train, val, and test data sets
-def load_and_prepare_data(file_path):
+def load_and_prepare_data(file_path, MAX_WORDS, MAX_LEN):
 
     # import data
     train_df = pd.read_csv(file_path, engine='python')
 
     # get labels
     labels = train_df.drop(columns=['id','comment_text']).columns.to_list()
-
-    # Get the 20000 most common words to tokenize
-    MAX_WORDS = 20000
-    # For each comment, we want the length to be 200. If it is more, it will be cut short, if it is less, it will be padded
-    MAX_LEN = 200
 
     # create Keras tokenizer and set num_words parameter
     tokenizer = Tokenizer(num_words=MAX_WORDS)
@@ -52,37 +48,43 @@ def load_and_prepare_data(file_path):
         maxlen=MAX_LEN
         )
 
-
-    # # toeknize 20000 most common words (remove symbols and excess spaces)
-    # tokenizer.fit_on_texts(train_df['comment_text'].apply(clean_text))
-
-    # # convert each comment to a sequence of 200 numeric word ID's
-    # X_data = pad_sequences(tokenizer.texts_to_sequences(train_df['comment_text']), maxlen=MAX_LEN)
-
     # select the label columns for our y_train ds and convert to numpy matrix where each row corresponds to a single comments labels
     # make data type float for gradient calculation and creating sample weights
     y_data = train_df[labels].values.astype('float32')
 
     # using iterative stratification, which balances the label combinations across splits.
-    X_train, y_train, X_test, y_test = iterative_train_test_split(X_data, y_data, test_size=0.2)
 
-    # using iterative stratification, which balances the label combinations across splits.
-    X_train, y_train, X_val, y_val = iterative_train_test_split(X_train, y_train, test_size=0.25)
+    X_train, y_train, X_test, y_test = iterative_train_test_split(
+        X_data, 
+        y_data, 
+        test_size=0.2
+    )
 
-    # make data type float for gradient calculation and creating sample weights in next step
-    # y_train = y_train.astype('float32')
-    # y_val = y_val.astype('float32')
-    # y_test = y_test.astype('float32')
- 
+    X_train, y_train, X_val, y_val = iterative_train_test_split(
+        X_train, 
+        y_train, 
+        test_size=0.25
+    )
+
     class_totals = np.sum(y_train, axis=0)
 
     total_samples = y_train.shape[0]
 
     class_weights = {i: total_samples / (len(labels) * class_totals[i]) for i in range(len(labels))}
 
+    # new
     sample_weights = np.ones_like(y_train, dtype='float32')
     for i in range(len(labels)):
-        sample_weights[:, i] = y_train[:, i] * class_weights[i]
+        sample_weights[:, i] = (
+            y_train[:, i] * class_weights[i] + 
+            (1 - y_train[:, i]) * 1.0  
+            )
+        
+        #sample_weights[:, i] = y_train[:, i] * class_weights[i]
+
+
+
+
 
     sample_weights_flat = np.mean(sample_weights, axis=1).astype('float32')
 
@@ -97,26 +99,48 @@ def load_and_prepare_data(file_path):
 
 
 # create funciton to build our deep learning model
+# def build_model(MAX_WORDS, MAX_LEN, labels):
+#     model = Sequential([
+#         Embedding(input_dim=MAX_WORDS + 1, output_dim=128, input_length=MAX_LEN),
+#         LSTM(128, recurrent_activation='sigmoid', use_bias=True),
+#         Dropout(0.2),
+#         Dense(len(labels), activation='sigmoid', dtype='float32')
+#     ])
+
+#     model.compile(
+#         optimizer='adam',
+#         loss='binary_crossentropy',
+#         metrics=['accuracy']  # list or dict matching output names
+#     )
+    
+#     return model
+
+
+
 def build_model(MAX_WORDS, MAX_LEN, labels):
     model = Sequential([
-        Embedding(input_dim=MAX_WORDS + 1, output_dim=128, input_length=MAX_LEN),
-        LSTM(128, recurrent_activation='sigmoid', use_bias=True),
-        Dropout(0.2),
+        Embedding(MAX_WORDS + 1, 200, input_length=MAX_LEN),
+        Bidirectional(LSTM(64, return_sequences=True, dtype='float32')),  # force FP32
+        GlobalMaxPooling1D(),
+        Dense(64, activation='relu'),
+        Dropout(0.3),
         Dense(len(labels), activation='sigmoid', dtype='float32')
     ])
+
 
     model.compile(
         optimizer='adam',
         loss='binary_crossentropy',
-        metrics=['accuracy']  # list or dict matching output names
+        metrics=[tf.keras.metrics.AUC(curve="ROC", multi_label=True)]
     )
-    
     return model
+
 
 def build_callbacks():
     callbacks = [
-        EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True),
-        ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=2, min_lr=1e-6)
+        ModelCheckpoint("best_model.keras", monitor="val_loss", save_best_only=True, save_weights_only=False),
+        ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=2, min_lr=1e-6),
+        EarlyStopping(monitor="val_loss", patience=3, restore_best_weights=True)
     ]
 
     return callbacks
@@ -125,13 +149,11 @@ def fit_model(model, train_ds, val_ds, callbacks):
     history = model.fit(
         train_ds,
         validation_data=val_ds,
-        epochs=10,
+        epochs=20,
         callbacks=callbacks
     )
 
-    model.save("model.keras")
-
-    return history
+    return model, history
 
 def evaluate_model(model, test_ds):
     test_accuracy, test_loss = model.evaluate(test_ds)
@@ -141,20 +163,20 @@ def main():
     # Get the 20000 most common words to tokenize
     MAX_WORDS = 20000
     # For each comment, we want the length to be 200. If it is more, it will be cut short, if it is less, it will be padded
-    MAX_LEN = 200
+    MAX_LEN = 300
 
     # get training, validation, and test data (tensorflow dataset objects)
-    train_ds, val_ds, test_ds, labels = load_and_prepare_data('train.csv')
+    train_ds, val_ds, test_ds, labels = load_and_prepare_data('train.csv', MAX_WORDS, MAX_LEN)
 
     model = build_model(MAX_WORDS, MAX_LEN, labels)
 
     callbacks = build_callbacks()
 
-    fit_model(model, train_ds, val_ds, callbacks)
+    model, history = fit_model(model, train_ds, val_ds, callbacks)
 
     # imported_model = keras.models.load_model("model.keras")
 
-    evaluate_model(test_ds)
+    evaluate_model(model, test_ds)
 
 if __name__ == "__main__":
     main()
