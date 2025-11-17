72146776801b970175b057eac2521bebd148c56d
Chris <chris.r.thompson1212@gmail.com>
2025-11-17 13:25:10 -0500

    update file for testing

--- build_model_wandb.py
+++ build_model_wandb.py
@@ -262,7 +262,7 @@ def main():
             "lstm_units": 64,
             "dropout": 0.3,
             "batch_size": 128,
-            "epochs": 1,
+            "epochs": 2,
             "optimizer": "adam"
         }
     )
