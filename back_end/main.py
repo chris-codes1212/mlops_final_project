b6122d671d40207033eb311fafd12c6811b3b238
Chris <chris.r.thompson1212@gmail.com>
2025-11-17 11:38:47 -0500

    making fastapi backend

---
+++ back_end/main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from datetime import datetime
+import pandas as pd
+import json
+import os
+import sklearn
+
+import utils
+
+
+# create FastAPI app
+app = FastAPI(
+    title="Toxic Comment Moderation"
+)
+
+# try and load model and tokenizer pipeline, if not successful, print error message
+try:
+    ENTITY = 'chris-r-thompson1212-university-of-denver'
+    PROJECT = "toxic-comment-multilabel"
+    model, tokenizer = utils.load_production_model_and_tokenizer(ENTITY, PROJECT)
+    print("Model Loaded Successfull")
+except FileNotFoundError:
+    print("Error: Model file 'sentiment_model.pkl' not found.")
+    model = None
+    tokenizer = None
+
+
+# create a class for the /predict endpoint
+class predict_input(BaseModel):
+    comment: str
+    # true_label: str
+
+# create a class for the logs we will create
+# class log(BaseModel):
+#     timestamp: datetime
+#     request_text: str
+#     # predicted_label: str
+#     # true_label: str
+
+
+
+# create startup event to print if model is not loaded
+@app.on_event("startup")
+def startup_event():
+    if model is None:
+        print("WARNING: Model is not loaded. Prediction endpoints will not work properly")
+
+# create health get endpoint to show 'status: ok' to confirm API is working
+@app.get('/health')
+async def root():
+    return{'status': 'ok'}
+
+# create predict endpoint to make predictions using loaded model
+@app.post("/predict")
+async def make_prediction(input_data: predict_input):
+
+    # if model did not load properly, give 503 error
+    if model is None:
+        raise HTTPException(
+            status_code = status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail = 'Model is not loaded. Cannot make predictions'
+        )
+    
+    # # turn input text into an array of words for correct model input data type
+    # text_array = [input_data.comment]
+
+    # make prediction with the model
+    prediction = model.predict(utils.preprocess_user_input(input_data.comment, tokenizer))
+    
+    # write new log to logs file by calling write_logs function
+    # utils.write_logs(input_data, prediction)
+
+    # return the prediction from the model
+    return {'Predicted Probabilities': prediction}
---
+++ back_end/utils.py
@@ -0,0 +1,120 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pandas as pd
+import tensorflow as tf
+
+import wandb
+import pickle
+import re
+
+def load_production_model_and_tokenizer(entity, project, model_name="toxic-comment-multilabel"):
+    
+    # Login to W&B (will use WANDB_API_KEY env variable)
+    wandb.login()
+    
+    api = wandb.Api()
+
+    # Fetch the production artifact
+    artifact = api.artifact(
+        f"{entity}/{project}/{model_name}:production",
+        type="model"
+    )
+
+    # Download the artifact locally
+    artifact_path = artifact.download()
+
+    # Load Keras model
+    model_file = f"{artifact_path}/best_model.keras"
+    model = tf.keras.models.load_model(model_file)
+
+    # Load tokenizer
+    tokenizer_file = f"{artifact_path}/tokenizer.pkl"
+    with open(tokenizer_file, "rb") as f:
+        tokenizer = pickle.load(f)
+
+    return model, tokenizer
+
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+def preprocess_user_input(user_input, tokenizer):
+    user_input_cleaned = clean_text(user_input)
+    seq = tokenizer.texts_to_sequences([user_input_cleaned])
+    padded_seq = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=300)
+    return padded_seq
+
+# # create a funciton to handle writing json logs
+# def write_logs(input_data, prediction):
+
+#     # create new log object
+#     new_log = log( 
+#         timestamp = datetime.now(),
+#         request_text = input_data.text,
+#         predicted_label = prediction[0],
+#         true_label = input_data.true_label
+#         )
+
+#     # format log object as a json object
+#     new_log_json = new_log.model_dump_json()
+    
+#     # set log file path -- Using .jsonl b/c will be cleaner than using a list format
+#     file_path = "../logs/prediction_logs.ndjson"
+
+#     # check if logs directory exists, if not create it and logs file and write new log to file
+#     if not os.path.isdir("../logs"):
+#         os.makedirs("../logs", exist_ok=True)
+#         with open(file_path, "w") as log_file:
+#             log_file.write(new_log_json)
+#             return 
+
+#     # check if log file exists, if not, create it and write new log to file then return
+#     # also checks if file is empty, if so, write new log then return
+#     if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+#         with open(file_path, "w") as log_file:
+#             log_file.write(new_log_json)
+#             return 
+        
+#     # otherwise, write new log with new line
+#     else:
+#         existing_logs_list = []
+#         with open(file_path, "a") as log_file:
+#             log_file.write(f'\n{new_log_json}')
+
+
+# user_input = st.text_input("insert comment")
+
+# if st.button("predict"):
+
+#     user_input_cleaned = clean_text(user_input)
+#     seq = tokenizer.texts_to_sequences([user_input_cleaned])
+#     padded_seq = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=300)
+
+#     prediction = model.predict(padded_seq)
+#     st.write("Predicted Probabilities:", prediction)
+
+
+# if comment:
+#     # Preprocess comment (clean + tokenize + pad)
+#     def clean_text(text):
+#         import re
+#         text = text.lower()
+#         text = re.sub(r'\s+', ' ', text).strip()
+#         return text
+
+#     MAX_LEN = 300  # same as training
+#     cleaned_comment = clean_text(comment)
+#     seq = tokenizer.texts_to_sequences([cleaned_comment])
+#     padded_seq = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=MAX_LEN)
+
+#     # Predict
+#     prediction = model.predict(padded_seq)
+#     st.write("Predicted probabilities:", prediction)
+# # from sklearn.metrics import accuracy_score, precision_score
+
+
+
+# model = load_model('../model.keras')
