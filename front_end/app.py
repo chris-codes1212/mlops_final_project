50ff07cd463a0daf7afd972589e462ab78cde9a8
Chris <chris.r.thompson1212@gmail.com>
2025-11-18 15:08:25 -0500

    cleaning up the front end

--- front_end/app.py
+++ front_end/app.py
@@ -9,40 +9,26 @@ import wandb
 import pickle
 import re
 
+# create title and subtitle
+st.title("Comment Toxicity Classifier")
+st.subheader("A site to grade the toxicity of online comments")
+st.text("Enter a comment in the text box below. Press 'Submit' after typing a comment. You will see what classes of toxicity your comment falls into")
+
+# set url for the backend api (FastAPI) to make requests to
 back_end_url = "http://44.202.157.103:8000/predict"
 
+# get example comment from the user
 user_input = st.text_input("Insert comment")
 
+# if the user presses the 'Submit' button, we will give the user predictions for the toxicity labels of their comment
 if st.button("Predict"):
+    # create payload which needs to be in json format
     payload = {"comment": user_input}
+    # try to call the /predict Fastapi endpoint with the new comment
     try:
         response = requests.post(back_end_url, json=payload, timeout=5)
         response.raise_for_status()  # raise exception for HTTP errors
         json_response = response.json()
-        st.write(json_response)
+        st.write(json_response['labels'])
     except requests.exceptions.RequestException as e:
         st.error(f"Error connecting to backend: {e}")
-
-
-
-# if comment:
-#     # Preprocess comment (clean + tokenize + pad)
-#     def clean_text(text):
-#         import re
-#         text = text.lower()
-#         text = re.sub(r'\s+', ' ', text).strip()
-#         return text
-
-#     MAX_LEN = 300  # same as training
-#     cleaned_comment = clean_text(comment)
-#     seq = tokenizer.texts_to_sequences([cleaned_comment])
-#     padded_seq = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=MAX_LEN)
-
-#     # Predict
-#     prediction = model.predict(padded_seq)
-#     st.write("Predicted probabilities:", prediction)
-# # from sklearn.metrics import accuracy_score, precision_score
-
-
-
-# model = load_model('../model.keras')
