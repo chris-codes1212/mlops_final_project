700f182176374262cc3ed3ad552c7b8be4015dde
Chris <chris.r.thompson1212@gmail.com>
2025-11-18 13:12:32 -0500

    updating streamlit front end for testing connection to /predict fastapi endpoint

--- front_end/app.py
+++ front_end/app.py
@@ -1,68 +1,29 @@
 import streamlit as st
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import pandas as pd
 import tensorflow as tf
-
+import requests
 import wandb
 import pickle
 import re
 
-def load_production_model_and_tokenizer(entity, project, model_name="toxic-comment-multilabel"):
-    
-    # Login to W&B (will use WANDB_API_KEY env variable)
-    wandb.login()
-    
-    api = wandb.Api()
-
-    # Fetch the production artifact
-    artifact = api.artifact(
-        f"{entity}/{project}/{model_name}:production",
-        type="model"
-    )
-
-    # Download the artifact locally
-    artifact_path = artifact.download()
-
-    # Load Keras model
-    model_file = f"{artifact_path}/best_model.keras"
-    model = tf.keras.models.load_model(model_file)
-
-    # Load tokenizer
-    tokenizer_file = f"{artifact_path}/tokenizer.pkl"
-    with open(tokenizer_file, "rb") as f:
-        tokenizer = pickle.load(f)
-
-    return model, tokenizer
-
-
-def clean_text(text):
-    text = text.lower()
-    text = re.sub(r'\s+', ' ', text).strip()
-    return text
-
-st.title('Toxicity Analysis App')
-
-st.write("Loading production model...")
-
-ENTITY = 'chris-r-thompson1212-university-of-denver'
-PROJECT = "toxic-comment-multilabel"
-model, tokenizer = load_production_model_and_tokenizer(ENTITY, PROJECT)
-
-st.success("Model Loaded!")
+back_end_url =  "http://44.202.157.103:8000/predict" 
 
 user_input = st.text_input("insert comment")
 
 if st.button("predict"):
+    payload = {'comment': user_input}
+    try:
+        # make call to /predict endpoint
+        response = requests.post(back_end_url, json=payload)
 
-    user_input_cleaned = clean_text(user_input)
-    seq = tokenizer.texts_to_sequences([user_input_cleaned])
-    padded_seq = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=300)
+    except requests.exceptions.RequestException as e:
+        st.text(f"An error occurred: {e}")
+        response = "No response from back end"
 
-    prediction = model.predict(padded_seq)
-    st.write("Predicted Probabilities:", prediction)
+    # prediction = model.predict(padded_seq)
+    st.write(response)
 
 
 # if comment:
