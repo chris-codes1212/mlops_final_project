62197024c5421d6106c6010723d38a1bb01825b6
Chris <chris.r.thompson1212@gmail.com>
2025-11-25 11:15:31 -0500

    cleanup write_logs.py

--- back_end/write_logs.py
+++ back_end/write_logs.py
@@ -2,20 +2,25 @@ import boto3
 from datetime import datetime
 from decimal import Decimal
 
-# connect to DynamoDB table
+# Connect to DynamoDB table
 dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
 table = dynamodb.Table("toxicity_app")
-    
-# create a funciton to handle writing json logs to DyanmoDB
+
+
+# Create a function to handle writing JSON logs to DynamoDB
 def write_log(input_data, pred_labels, pred_proba_dict, latency, labels=None):
-    # convert pred_proba to decimal values
+    # Convert pred_proba to decimal values
     decimal_pred_proba = {k: Decimal(str(v)) for k, v in pred_proba_dict.items()}
-    # send log to DynamoDB
-    table.put_item(Item={"timestamp": datetime.now().astimezone().isoformat(), 
-                         "comment": input_data.comment,
-                         "prediction_labels": pred_labels,
-                         "predicted_proba": decimal_pred_proba,
-                         "latency_seconds": Decimal(str(latency))
-                         })
-    
+
+    # Send log to DynamoDB
+    table.put_item(
+        Item={
+            "timestamp": datetime.now().astimezone().isoformat(),
+            "comment": input_data.comment,
+            "prediction_labels": pred_labels,
+            "predicted_proba": decimal_pred_proba,
+            "latency_seconds": Decimal(str(latency)),
+        }
+    )
+
     return
