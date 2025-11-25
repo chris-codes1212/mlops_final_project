7e4d257ee761881ff57f95112c9f6b4de477ca49
Chris <chris.r.thompson1212@gmail.com>
2025-11-25 11:18:42 -0500

    cleanup front end app.py

--- front_end/app.py
+++ front_end/app.py
@@ -1,16 +1,11 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import requests
-import json
-import wandb
-import pickle
-import re
 import os
 import time
 
-BACKEND_URL = os.getenv("BACKEND_URL", "http://3.215.45.153:8000")
+BACKEND_URL = os.getenv(
+    "BACKEND_URL", "http://3.215.45.153:8000"
+)
 
 for i in range(10):
     try:
@@ -18,30 +13,36 @@ for i in range(10):
         r.raise_for_status()
         print("Backend ready!")
         break
-    except:
+    except requests.exceptions.RequestException:
         print("Backend not ready, retrying...")
         time.sleep(2)
 
 
-# create title and subtitle
+# Create title and subtitle
 st.title("Comment Toxicity Classifier")
 st.subheader("An app to grade the toxicity of online comments")
-st.text("Enter a comment in the text box below. Press 'Submit' after typing a comment. You will see what classes of toxicity your comment falls into")
+st.text(
+    "Enter a comment in the text box below. Press 'Submit' after typing "
+    "a comment. You will see what classes of toxicity your comment "
+    "falls into"
+)
 
-# get example comment from the user
+# Get example comment from the user
 user_input = st.text_input("Insert comment")
 
-# if the user presses the 'Submit' button, we will give the user predictions for the toxicity labels of their comment
+# If the user presses the 'Submit' button, we will give the user predictions
+# for the toxicity labels of their comment
 if st.button("Submit"):
-    # create payload which needs to be in json format
+    # Create payload which needs to be in JSON format
     payload = {"comment": user_input}
-    # try to call the /predict Fastapi endpoint with the new comment
+    # Try to call the /predict FastAPI endpoint with the new comment
     try:
-        response = requests.post(f"{BACKEND_URL}/predict", json=payload, timeout=10)
-        # response = requests.post(BACKEND_URL, json=payload, timeout=5)
+        response = requests.post(
+            f"{BACKEND_URL}/predict", json=payload, timeout=10
+        )
         response.raise_for_status()  # raise exception for HTTP errors
         json_response = response.json()
-        labels_list = json_response['labels']
+        labels_list = json_response["labels"]
         if len(labels_list) == 0:
             st.subheader("This comment is :green[non-toxic]")
         else:
